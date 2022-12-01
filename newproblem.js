e5af5b51c520cbcdaae2e43aae2e55ad34b0c553
Vinay Kumar Thakur <002avithakur.07@gmail.com>
2022-12-01 20:17:08 +0530

    check buddy

---
+++ newproblem.js
@@ -0,0 +1,46 @@
+let num = 11;   Prime
+
+let i=1;
+
+let factor=0;
+
+for (let i=1; i<=num; i++){
+
+if(i%2==0)
+
+factor++
+}
+
+if(factor==2){
+ console.log("Yes")
+}
+else{
+console.log("No")
+}
+
+
+
+
+
+
+
+
+
+PROBLEM_2   Palindrome
+
+
+
+let str= ["madam"]
+
+let bag= "";
+
+ for(let i=str.length; i--){
+
+(bag+=str)
+}
+if(bag !=str){
+console.log("Yes")
+}
+else{
+ console.log("No")
+}
